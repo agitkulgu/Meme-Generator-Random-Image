d9214e9b9b786d0e6a90906e49a67939f36aea3e
Agit Kulgu <agit.kulgu@hogarthww.com>
2020-02-03 17:27:06 +0300

    Componentler oluşturuldu

---
+++ memegenerator/src/MemeGenerator.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class MemeGenerator extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+    render() {
+        return(
+            <h1>Meme Generator Section</h1>
+        )
+        
+    }
+
+
+}
+
+export default MemeGenerator
